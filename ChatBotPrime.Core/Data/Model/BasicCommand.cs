c1acb05830b890eccb3e3cf36f070ca160614fb4
CrypticEnigma <mburns5911@hotmail.co.uk>
2020-07-11 10:13:25 +0100

    Added BasicCommand Entity for use with handler system

---
+++ ChatBotPrime.Core/Data/Model/BasicCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatBotPrime.Core.Data.Model
+{
+	public class BasicCommand	: DataEntity
+	{
+
+	}
+}
---
+++ ChatBotPrime.Core/Data/Specifications/BasicCommandPolicy.cs
@@ -0,0 +1,15 @@
+﻿using ChatBotPrime.Core.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ChatBotPrime.Core.Data.Specifications
+{
+	public class BasicCommandPolicy : DataItemPolicy<BasicCommand>
+	{
+		protected BasicCommandPolicy(Expression<Func<BasicCommand, bool>> expression) : base(expression)
+		{
+		}
+	}
+}
